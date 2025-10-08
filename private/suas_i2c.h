2cdd3bcbd742cdc8fe607abf0b1c4fde6fea4f56
Tobias Tefke <tobias@tefke.name>
2025-10-08 12:02:11 +0200

    I2C: Ensure we have the function prototype required

--- private/suas_i2c.h
+++ private/suas_i2c.h
@@ -1,6 +1,8 @@
 #ifndef __SUAS_I2C_H
 #define __SUAS_I2C_H
 
+#include <hal_i2c.h>
+
 // Macros to ease function access
 #define suas_write_byte_i2c(device, data, address) \
     hal_i2c_write_block(device, data, 1, 1, address)
