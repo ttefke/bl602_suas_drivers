daeb58416186190d889d221b366346ebaf8be256
Tobias Tefke <tobias@tefke.name>
2025-10-11 10:23:02 +0200

    RFID: correct naming of variables and reset frequently

--- include/suas_rfid.h
+++ include/suas_rfid.h
@@ -2,15 +2,15 @@
 #define __SUAS_RFID_H
 
 // Data structure to get the uid from the card
-typedef struct suas_rfc_uid_t {
+typedef struct suas_rfid_uid_t {
     uint8_t size;
     uint8_t uid_byte[10];
-} suas_rfc_uid_t;
+} suas_rfid_uid_t;
 
 // Function prototypes
 void suas_rfid_init();
 uint8_t suas_rfid_is_new_card_present();
 bool suas_rfid_read_card();
-suas_rfc_uid_t suas_rfid_get_uid();
+suas_rfid_uid_t suas_rfid_get_uid();
 
 #endif
--- rfid.cpp
+++ rfid.cpp
@@ -8,6 +8,8 @@ extern "C" {
 
 // Porting layer allowing to use the Arduino rfid library on the PineCone
 static MFRC522 rfid(SS, 5);
+static uint8_t nr_of_scans = 0;
+
 extern SerialClass Serial;
 extern SPIClass SPI;
 
@@ -20,6 +22,11 @@ extern "C" void suas_rfid_init() {
 
 // Check if new card is present
 extern "C" uint8_t suas_rfid_is_new_card_present() {
+    /* Reset frequently to avoid some glitches */
+    if (nr_of_scans == 127) {
+        nr_of_scans = 0;
+        suas_rfid_init();
+    }
     return rfid.PICC_IsNewCardPresent();
 }
 
@@ -29,8 +36,8 @@ extern "C" bool suas_rfid_read_card() {
 }
 
 // Get UID from card
-extern "C" suas_rfc_uid_t suas_rfid_get_uid() {
-    suas_rfc_uid_t retval;
+extern "C" suas_rfid_uid_t suas_rfid_get_uid() {
+    suas_rfid_uid_t retval;
     retval.size = rfid.uid.size;
     memcpy(retval.uid_byte, rfid.uid.uidByte, rfid.uid.size);
     return retval;
